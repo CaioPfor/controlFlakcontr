055b9c626fc36b3d7c2a5df4554ea66ff29ca86a
CaioPfor <caioanjosportela@gmail.com>
2022-05-30 18:07:59 +0200

    confrirmar

--- controler.py
+++ controler.py
@@ -1,7 +1,47 @@
 from app import app
 import os
+from flask import Flask, request, jsonify, send_from_directory
 
 
 if __name__=='main':
     port = int(os.getenv('PORT'), '5000')
     app.run(host='0.0.0.0', port= port)
+
+
+
+DIRETORIO = "C:\\xampp\\htdocs\\controlFlak"
+
+api = Flask(__name__)
+
+
+@api.route("/arquivos", methods=["GET"])
+def lista_arquivos():
+    arquivos = []
+
+    for nome_do_arquivo in os.listdir(DIRETORIO):
+        endereco_do_arquivo = os.path.join(DIRETORIO, nome_do_arquivo)
+
+        if(os.path.isfile(endereco_do_arquivo)):
+            arquivos.append(nome_do_arquivo)
+
+    return jsonify(arquivos)
+
+
+@api.route("/arquivos/<nome_do_arquivo>",  methods=["GET"])
+def get_arquivo(nome_do_arquivo):
+    return send_from_directory(DIRETORIO, nome_do_arquivo, as_attachment=True)
+
+
+@api.route("/arquivos", methods=["POST"])
+def post_arquivo():
+    arquivo = request.files.get("meuArquivo")
+
+    print(arquivo)
+    nome_do_arquivo = arquivo.filename
+    arquivo.save(os.path.join(DIRETORIO, nome_do_arquivo))
+
+    return '', 201
+
+
+if __name__ == "__main__":
+    api.run(debug=True, port=5000)    
